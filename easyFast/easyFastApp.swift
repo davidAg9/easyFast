a8a9947a5c12a9163e55456e3b51eb246c480cba
davidAg9 <shane_alpha9@outlook.com>
2022-03-25 15:18:21 +0000

    Initial Commit

---
+++ easyFast/easyFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyFastApp.swift
+//  easyFast
+//
+//  Created by Dave Agyakwa on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct easyFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
